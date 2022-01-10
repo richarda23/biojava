670dc2f81dcd077044df0414cd26f33a80538aa4
richard <ra22597@gmail.com>
2022-01-10 21:52:19 +0000

    test delete term/triple

--- biojava-ontology/src/test/java/org/biojava/nbio/ontology/OntologyTest.java
+++ biojava-ontology/src/test/java/org/biojava/nbio/ontology/OntologyTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OntologyTest {
@@ -38,6 +40,23 @@ class OntologyTest {
     }
 
     @Test
+    @DisplayName("getTerms is encapsulated")
+    void getTerms() throws AlreadyExistsException {
+        Triple triple = createAnyTriple("s","o","p");
+        // 3 terms + 1 triple
+        Set<Term> terms = anyOntology.getTerms();
+        assertEquals(4, terms.size());
+
+        terms.remove(triple.getSubject());
+        assertEquals(3, terms.size());
+        // still 4 terms in the ontology
+        assertEquals(4, anyOntology.getTerms().size());
+
+
+
+    }
+
+    @Test
     void containsTriple() throws AlreadyExistsException {
         Triple triple = createAnyTriple("s", "o", "p");
         Triple triple2 = createAnyTriple("s2", "o2", "p2");
@@ -60,6 +79,7 @@ class OntologyTest {
         assertTrue(anyOntology.containsTerm(triple.getName()));
         anyOntology.deleteTerm(triple);
         assertFalse(anyOntology.containsTerm(triple.getName()));
+
         assertTrue(anyOntology.containsTerm(triple.getSubject().getName()));
         assertTrue(anyOntology.containsTerm(triple.getObject().getName()));
         assertTrue(anyOntology.containsTerm(triple.getPredicate().getName()));
@@ -72,7 +92,20 @@ class OntologyTest {
         anyOntology.deleteTerm(triple.getSubject());
         assertTrue(anyOntology.containsTerm(triple.getName()));
         assertFalse(anyOntology.containsTerm(triple.getSubject().getName()));
+        // we just deleted this subject term but triples containing the subject are still present
+        // is this correct?
         assertEquals(1, anyOntology.getTriples(triple.getSubject(),null, null).size());
+    }
+
+    @Test
+    @DisplayName("Synonyms are encapsulated in terms")
+    void createTermWithSynonyms() throws AlreadyExistsException {
+        String [] synonyms = new String[]{"alias1", "alias2"};
+        Term t = anyOntology.createTerm("s1", "desc", synonyms);
+        assertEquals("alias2", t.getSynonyms()[1]);
+        synonyms[1] = "XXXX";
+        assertEquals("alias2", t.getSynonyms()[1]);
+
     }
 
     private Triple createAnyTriple(String subjectName, String objectName, String predName) throws AlreadyExistsException {
